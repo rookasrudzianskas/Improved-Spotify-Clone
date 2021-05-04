5dea0d88f8e858e9ce79cb68cdaa2c883806f751
Rokas <rookas.rudzenskas@gmail.com>
2021-05-04 22:06:32 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Login from "./components/Login";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {getTokenFromUrl} from "./components/spotify";
 
 function App() {
+    // we store the token in the state
+    const [token, setToken] = useState(null)
 
     //    run the code on the condition
     useEffect(() => {
@@ -12,9 +14,19 @@ function App() {
         // the app loads for the first time, the token is undefined, but then I click agree on the login,
         // the app component gets rerendered and we run the useEffect one more time, in this case it gets token from the url
         // and returns in
-        const token = getTokenFromUrl();
-        console.log("I HAVE THE TOKEEEN 🚀", token)
-    })
+        const hash = getTokenFromUrl();
+        // this is going to go the url, and clear everything after the hash
+        window.location.hash = "";
+        // the token is inside the hash, and we access it, by going to hash object, and then accessing the token
+        // we use _, to show that is is like temporary and the real one is then we set it to the state, and the state token is ther real one
+        const _token = hash.access_token;
+
+        if(_token) {
+            // if there is a token, so we set it up to the token in the state, using setState
+            setToken(_token)
+        }
+        console.log("I HAVE THE TOKEEEN 🚀", _token)
+    }, []);
 
 
 
