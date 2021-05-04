330b0fbdbd3fdab32f25a505ff37d2da70e1fdf1
Rokas <rookas.rudzenskas@gmail.com>
2021-05-04 19:00:27 +0300

    Rokas

---
+++ src/components/spotify.js
@@ -0,0 +1,6 @@
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+// where we are going to send the user to make the login
+//spotify takes care of login, and then we go
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = "77b7d9ed0a404694b7c7b0dc75d10051";
