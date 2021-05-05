9e2e1dabdf3326c5b5e09cd69e38357bb584e00e
Rokas <rookas.rudzenskas@gmail.com>
2021-05-05 08:24:15 +0300

    Rokas

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,13 @@ import React from 'react';
 import "./styles/Header.css";
 import { Avatar } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
+import {useDataLayerValue} from "../DataLayer";
 
 
 const Header = () => {
+    // we are accessing the data layer, to receive the needed info
+    const [{ user }, dispatch] = useDataLayerValue();
+
     return (
         <div className="header">
             <div className="header__left">
@@ -16,8 +20,10 @@ const Header = () => {
             </div>
 
             <div className="header__right">
-                <Avatar alt="" src="" />
-                <h4>Rokas Rudzianskas</h4>
+                {/* we go to the user object in data layer and get the image url first one*/}
+                <Avatar alt={user?.display_name} src={user?.images[0]?.url} />
+                {/* we go to the usr oberject from the data layer, adn get the display name*/}
+                <h4>{user?.display_name}</h4>
             </div>
         </div>
     );
