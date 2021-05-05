d33c5e9f0162b3a2d264fd38736953f4de9676bb
Rokas <rookas.rudzenskas@gmail.com>
2021-05-05 07:06:07 +0300

    Rokas

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import "./styles/Sidebar.css";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
+import SidebarOption from "./SidebarOptions";
 
 const Sidebar = () => {
     return (
@@ -9,6 +13,12 @@ const Sidebar = () => {
                 src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
                 alt=""
             />
+
+            <SidebarOption Icon={HomeIcon} title="Home" />
+            <SidebarOption Icon={SearchIcon} title="Search" />
+            <SidebarOption Icon={LibraryMusicIcon} title="Your Library" />
+            <br />
+
         </div>
     );
 };
