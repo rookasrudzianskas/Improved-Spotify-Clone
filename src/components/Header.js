f006e81b5b48dd4622c9c065a63bd64c1fdac22c
Rokas <rookas.rudzenskas@gmail.com>
2021-05-05 08:20:12 +0300

    Rokas

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,19 @@ import SearchIcon from "@material-ui/icons/Search";
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className="header">
+            <div className="header__left">
+                <SearchIcon />
+                <input
+                    placeholder="Search for Artists, Songs, or Podcasts "
+                    type="text"
+                />
+            </div>
+
+            <div className="header__right">
+                <Avatar alt="" src="" />
+                <h4>Rokas Rudzianskas</h4>
+            </div>
         </div>
     );
 };
