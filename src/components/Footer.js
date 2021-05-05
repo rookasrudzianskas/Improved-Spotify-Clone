afafc94a6ecfeeec3dae4329271ad73f1cc7d738
Rokas <rookas.rudzenskas@gmail.com>
2021-05-05 07:48:04 +0300

    Rokas

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,17 @@ import "./styles/Footer.css";
 const Footer = () => {
     return (
         <div className="footer">
-            <h1>I am the footer</h1>
+            <div className="footer__left">
+                <p>Album and song details</p>
+            </div>
+
+            <div className="footer__center">
+                <p>Player controles</p>
+            </div>
+
+            <div className="footer__right">
+                <p>Volume controls</p>
+            </div>
 
         </div>
     );
