6e9ac2902bedc91786a18a6ac9526ce8c3280b58
Rokas <rookas.rudzenskas@gmail.com>
2021-05-05 08:33:27 +0300

    Rokas

--- src/components/Body.js
+++ src/components/Body.js
@@ -7,6 +7,15 @@ const Body = ({ spotify }) => {
         <div className="body">
             <Header spotify={spotify} />
 
+            <div className="body__info">
+                <img src="https://i.pinimg.com/originals/cc/f7/b2/ccf7b23b67fc46919be6ac07634d5bfc.jpg" alt="" />
+                <div className="body__infoText">
+                    <strong>PLAYLIST</strong>
+                    <h2>Discover Weekly</h2>
+                    <p>description</p>
+                </div>
+            </div>
+
         </div>
     );
 };
